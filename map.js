ae39c5673938601ba7cce06e153307c4d957247f
Tristan Hatfield <tghatfield2005@gmail.com>
2025-04-14 12:04:08 -0500

    Picture Slider and Map
    Added picture slider on about me page
      - With two extra pictures
    Added working map onto the map page
      - Added a marker with an information card when you click on it.

--- map.js
+++ map.js
@@ -1,9 +1,45 @@
+// Initialize and add the map
 let map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+async function initMap() {
+  const position = { lat: 41.83300336533221, lng: -87.62450057900963 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("map"), {
+    zoom: 15,
+    center: position,
+    mapId: "map",
   });
-  console.log("Map initialized");
+const contentString =
+    "<div>" +
+    "<h1>Alpha Sigma Phi</h1>" +
+    "<div>" +
+    "<p><b>Alpha Sigma Phi</b>, known as <b>A Sigs</b> on campus, is a national " +
+    "fraternity with five cores values. Silence, Honor, Charity, Purity, and Patriotism." +
+    " We have chapters of the fraternity all over the United States and have been around since 1845.</p>" +
+    '<p>Visit the link to learn more about <a href="https://alphasig.org/">' +
+    "Alpha Sigma Phi </a> "
+    "</div>" +
+    "</div>";
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    ariaLabel: "Alpha Sigma Phi Fraternity House",
+  });
+	
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Alpha Sigma Phi",
+  });
+	marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+    });
+  });
 }
+
+initMap();
